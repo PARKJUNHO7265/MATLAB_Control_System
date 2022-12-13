897dadaf263e094372c6f291d6c8dfd35a5bcd8d
박준호 <87568714+PARKJUNHO7265@users.noreply.github.com>
2022-12-13 21:46:34 +0900

    Create Parameters.m

---
+++ Parameters.m
@@ -0,0 +1,9 @@
+% Panel Parameters
+Kd = 5;               % Damping constant, [N*m/(rad/s)]
+J = 8.6;
+
+% Motor parameters
+Kf = 0.07;             % Back EMF cosntant, [V/(rad/s)]
+Kt = 0.07;             % Torque constant, [N*m/A]
+R = 10;                % Resistance, [Ohm]
+Kg = 2000;             % Gear ratio, []
